8d46a443ad86db38e74977e33fdf919d4020e719
gblandz <gblandz@live.com>
2018-02-20 09:57:49 +0800

    create timekeeping page

---
+++ database/migrations/2018_02_19_234259_create_times_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->time('duration');
+            $table->integer('task_id')->unsigned();
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}

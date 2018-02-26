28cc33eb1d41e3cf01f551f307a57a6ebe2fcb3b
Angelo Blando <gblandz@live.com>
2018-02-26 14:10:46 +0800

    Revert "Merge to master"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 Route::get('/', function () { return redirect('/admin/home'); });
 
+// Task Routes..
+Route::get('/admin/tasks', 'TasksController@index')->name('admin.tasks.index');
+Route::get('/admin/tasks/add','TasksController@add')->name('admin.tasks.add');
+Route::post('/admin/tasks','TasksController@create')->name('admin.tasks.create');
+Route::get('/admin/tasks/{task}','TasksController@edit')->name('admin.tasks.edit');
+Route::post('/admin/tasks/{task}','TasksController@update')->name('admin.tasks.update');
+
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
 $this->post('login', 'Auth\LoginController@login')->name('auth.login');
@@ -24,6 +31,9 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
     Route::post('roles_mass_destroy', ['uses' => 'Admin\RolesController@massDestroy', 'as' => 'roles.mass_destroy']);
     Route::resource('users', 'Admin\UsersController');
     Route::post('users_mass_destroy', ['uses' => 'Admin\UsersController@massDestroy', 'as' => 'users.mass_destroy']);
+
     Route::resource('timekeeping', 'TimesController');
     Route::resource('chemicals', 'ChemicalsController');
+
+
 });

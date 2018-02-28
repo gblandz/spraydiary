97e6e3c908ef239c821fdc438eae080da5d14297
gblandz <gblandz@live.com>
2018-02-28 16:12:25 +0800

    edit migration table for times

--- database/migrations/2018_02_19_234259_create_times_table.php
+++ database/migrations/2018_02_19_234259_create_times_table.php
@@ -15,11 +15,21 @@ class CreateTimesTable extends Migration
     {
         Schema::create('times', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('task_id')->unsigned();
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->integer('block_id')->unsigned();
+            $table->foreign('block_id')->references('id')->on('blocks');
+            $table->string('sheds');
+            $table->integer('chemical_id')->unsigned();
+            $table->integer('chemical_id')->references('id')->on('chemicals');
+            $table->integer('tank_capacity');
+            $table->integer('total_liquid');
+            $table->string('sprayed_by');
+            $table->string('is_fruiting');
+            $table->string('audit_check');
             $table->time('start_time');
             $table->time('end_time');
             $table->time('duration');
-            $table->integer('task_id')->unsigned();
-            $table->foreign('task_id')->references('id')->on('tasks');
             $table->timestamps();
         });
     }

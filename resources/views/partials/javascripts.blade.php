34f3ef010199d3d7d59d5c708066fe189a5f7e11
gblandz <gblandz@live.com>
2018-03-08 21:39:05 +0800

    use carbon

--- resources/views/partials/javascripts.blade.php
+++ resources/views/partials/javascripts.blade.php
@@ -74,7 +74,9 @@
 				$("#stop_btn").show();
 				$("#start_btn").hide();  
 				var token = $('meta[name="_token"]').attr('content');
-				var timetobesaved = $("#startTimeContainer").val();
+				var dt = new Date();
+				var timetobesaved = dt.getFullYear() + "-" + (dt.getMonth()+1) + "-" + dt.getDate() + " " +dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+				// var timetobesaved = $("#startTimeContainer").val();
 				var block_id = $("#block_id").val();
 				var task_id = $("#task_id").val();
 				var sheds = $("#sheds").val();

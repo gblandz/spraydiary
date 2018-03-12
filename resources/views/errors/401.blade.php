f3320be84847969f335480b66fd8933a4f483a6e
gblandz <gblandz@live.com>
2018-03-12 11:31:03 +0800

    add return to home link to error page

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -3,5 +3,8 @@
 @section('content')
 
 <h2>{{ $exception->getMessage() }}</h2>
+<a href="{{ url('/') }}"><i class="fa fa-arrow-left"></i>
+    <span class="title">Go Back</span>
+</a>
 
 @endsection
